b88fbdef64d30869891a6622d037f48a588048d9
ryan <ryanwkoch@gmail.com>
2019-07-08 14:48:58 -0400

    provisioning script updated with vendor, assets/, index.html, coming_soon.html, nucleo-icons.html

--- now-ui-kit-master/deploy-to-google.sh
+++ now-ui-kit-master/deploy-to-google.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 gulp # or grunt?
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./vendor gs://www.glass-is-lit.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./vendor gs://www.opendiagnosis.foundation/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./css/*.min.css gs://www.glass-is-lit.com/css/
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./assets/css/*.min.css gs://www.opendiagnosis.foundation/assets/css/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./js/*.min.js gs://www.glass-is-lit.com/js/
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./assets/scss/*.scss gs://www.opendiagnosis.foundation/assets/scss/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./device-mockups gs://www.glass-is-lit.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./assets gs://www.opendiagnosis.foundation/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./img gs://www.glass-is-lit.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./assets/img gs://www.opendiagnosis.foundation/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./index.html gs://www.glass-is-lit.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./index.html gs://www.opendiagnosis.foundation/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./sitemap.xml gs://www.glass-is-lit.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./coming_soon.html gs://www.opendiagnosis.foundation/
 
-gsutil iam ch allUsers:objectViewer gs://www.glass-is-lit.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./nucleo-icons.html gs://www.opendiagnosis.foundation/
+
+# gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./sitemap.xml gs://www.opendiagnosis.foundation/
+
+gsutil iam ch allUsers:objectViewer gs://www.opendiagnosis.foundation/
 
