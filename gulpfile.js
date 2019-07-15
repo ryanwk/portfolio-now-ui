3df317c51a5cc78d94d8ca110d615a34c4d2ddba
ryan <ryanwkoch@gmail.com>
2019-07-15 14:11:03 -0400

    added imgs for portfolio pieces to img/
    added portfolio piece documents and pdfs to assets/
    
    added commented out code for gulp task to create vendor folder

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,53 @@ var Paths = {
   SCSS: './assets/scss/**/**'
 };
 
+// Copy third party libraries from /node_modules into / vendor
+// gulp.task('vendor', function () {
+
+//   // Bootstrap
+//   gulp.src([
+//     './node_modules/bootstrap/dist/**/*',
+//     '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+//     '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+//   ])
+//     .pipe(gulp.dest('./vendor/bootstrap'))
+
+//   // Font Awesome
+//   gulp.src([
+//     './node_modules/font-awesome/**/*',
+//     '!./node_modules/font-awesome/{less,less/*}',
+//     '!./node_modules/font-awesome/{scss,scss/*}',
+//     '!./node_modules/font-awesome/.*',
+//     '!./node_modules/font-awesome/*.{txt,json,md}'
+//   ])
+//     .pipe(gulp.dest('./vendor/font-awesome'))
+
+//   // jQuery
+//   gulp.src([
+//     './node_modules/jquery/dist/*',
+//     '!./node_modules/jquery/dist/core.js'
+//   ])
+//     .pipe(gulp.dest('./vendor/jquery'))
+
+//   // jQuery Easing
+//   gulp.src([
+//     './node_modules/jquery.easing/*.js'
+//   ])
+//     .pipe(gulp.dest('./vendor/jquery-easing'))
+
+//   // Simple Line Icons
+//   gulp.src([
+//     './node_modules/simple-line-icons/fonts/**',
+//   ])
+//     .pipe(gulp.dest('./vendor/simple-line-icons/fonts'))
+
+//   gulp.src([
+//     './node_modules/simple-line-icons/css/**',
+//   ])
+//     .pipe(gulp.dest('./vendor/simple-line-icons/css'))
+
+// });
+
 gulp.task('compile-scss', function () {
   return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
     .pipe(sourcemaps.init())
