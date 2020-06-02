723d2a7cc9218894ac44d4cc76cc6cd54718cdc5
ryanwk <ryanwkoch@gmail.com>
2020-06-01 21:35:25 -0400

    toph tech intervention

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,59 @@
-var gulp = require('gulp');
-var path = require('path');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var open = require('gulp-open');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var path = require("path");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
+var open = require("gulp-open");
+var browserSync = require("browser-sync").create();
 
 var Paths = {
-  HERE: './',
-  DIST: 'dist/',
-  CSS: './assets/css/',
-  SCSS_TOOLKIT_SOURCES: './assets/scss/now-ui-kit.scss',
-  SCSS: './assets/scss/**/**'
+  HERE: "./",
+  DIST: "dist/",
+  CSS: "./assets/css/",
+  SCSS_TOOLKIT_SOURCES: "./assets/scss/now-ui-kit.scss",
+  SCSS: "./assets/scss/**/**",
 };
-gulp.task('compile-scss', gulp.series(function () {
-  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write(Paths.HERE))
-    .pipe(gulp.dest(Paths.CSS));
-}));
+gulp.task(
+  "compile-scss",
+  gulp.series(function () {
+    return gulp
+      .src(Paths.SCSS_TOOLKIT_SOURCES)
+      .pipe(sourcemaps.init())
+      .pipe(sass().on("error", sass.logError))
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write(Paths.HERE))
+      .pipe(gulp.dest(Paths.CSS));
+  })
+);
 
-gulp.task('watch', gulp.series(function () {
-  gulp.watch(Paths.SCSS, ['compile-scss']);
-}));
+gulp.task(
+  "watch",
+  gulp.series(function () {
+    gulp.watch(Paths.SCSS, ["compile-scss"]);
+  })
+);
 
-gulp.task('open', gulp.series(function () {
-  gulp.src('index.html')
-    .pipe(open());
-}));
+gulp.task(
+  "open",
+  gulp.series(function () {
+    gulp.src("index.html").pipe(open());
+  })
+);
 
 // Configure the browserSync task
-gulp.task('browserSync', gulp.series(function () {
-  var pathArray = Object.keys(Paths).map(function (path) {
-    return Paths[path];
-  });
+gulp.task(
+  "browserSync",
+  gulp.series(function () {
+    var pathArray = Object.keys(Paths).map(function (path) {
+      return Paths[path];
+    });
 
-  browserSync.init(pathArray, {
-    server: {
-      baseDir: './'
-    }
-  });
-}));
+    browserSync.init(pathArray, {
+      server: {
+        baseDir: "./",
+      },
+    });
+  })
+);
 
-gulp.task('dev', gulp.series(['open', 'watch', 'browserSync']));
+gulp.task("dev", gulp.series(["open", "watch", "browserSync"]));
